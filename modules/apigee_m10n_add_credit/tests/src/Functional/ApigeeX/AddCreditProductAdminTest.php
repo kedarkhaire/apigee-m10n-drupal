935ed60a1b0dc2529983b19200e6bb959f090fab
Kedar Khaire <kedark@google.com>
2025-04-01 16:22:43 +0530

    check maximum

--- modules/apigee_m10n_add_credit/tests/src/Functional/ApigeeX/AddCreditProductAdminTest.php
+++ modules/apigee_m10n_add_credit/tests/src/Functional/ApigeeX/AddCreditProductAdminTest.php
@@ -101,7 +101,8 @@ class AddCreditProductAdminTest extends AddCreditFunctionalTestBase {
     $this->submitForm([
       'title[0][value]' => $title,
       'variations[form][0][sku][0][value]' => 'SKU-ADD-CREDIT-10',
-      'variations[form][0][price][0][number]' => '10.00'
+      'variations[form][0][price][0][number]' => '10.00',
+      'variations[form][0][apigee_price_range][0][price_range][fields][maximum]' => '10.00'
     ], 'Save');
     $this->assertCssElementContains('h1.page-title', $title);
     $this->assertCssElementContains('div.messages--status', "The product {$title} has been successfully saved.");
